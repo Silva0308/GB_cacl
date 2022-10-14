49ccfa3e56c3939efc18c9751ba44b96ba08d40b
AndrewDar <geologalone@gmail.com>
2022-10-14 12:51:23 +0300

    sqrt created

---
+++ model_sqrt.py
@@ -0,0 +1,17 @@
+import math
+from math import sqrt
+
+x = 0
+y = 0
+
+def init(a,b):
+    global x
+    global y
+    x = a
+    y = b
+    
+    
+def sqrt(x):    
+    return math.sqrt(x)
+
+print(sqrt(25))
