d53e50b05ce32c2b9a53fc73095469c69943c5f6
Silva0308 <travinina_valery@mail.ru>
2022-10-16 12:04:56 +0300

    добавила обрабоку ошибки ввода

--- compl.py
+++ compl.py
@@ -3,18 +3,27 @@ y = 0
 
 
 def get_parts():
-    a = int(input("Введите действительную часть числа: "))
-    b = int(input("Введите мнимую часть числа: "))
     global x
     global y
-    x = a
-    y = b
+    while True:
+        a = (input("Введите действительную часть числа: "))
+        if not (a.isdigit()):
+            print('Ошибка. Повторите ввод')
+            continue
+        x = int(a)
+        break
+    while True:
+        b = (input("Введите мнимую часть числа: "))
+        if not (b.isdigit()):
+            print('Ошибка. Повторите ввод')
+            continue
+        y = int(b)
+        break
     return x, y
 
 
 def make_complex(a, b):
     num = complex(a, b)
     return num
-
 
 
