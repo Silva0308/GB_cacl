cd1dcbd1c4af9ac49eee2f3d988748b392151d42
Silva0308 <travinina_valery@mail.ru>
2022-10-16 19:28:46 +0300

    изменила тип данных

--- compl.py
+++ compl.py
@@ -4,13 +4,13 @@ def make_complex():
         if not (a.isdigit()):
             print('Ошибка. Повторите ввод')
             continue
-        a = float(a)
+        a = int(a)
         break
     while True:
         b = (input("Введите мнимую часть числа: "))
         if not (b.isdigit()):
             print('Ошибка. Повторите ввод')
             continue
-        b = float(b)
+        b = int(b)
         break
     return complex(a, b)
